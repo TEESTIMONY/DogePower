8c677841004b8a3b6ae5154f9e90d6d922e11487
TEESTIMONY <149002722+TEESTIMONY@users.noreply.github.com>
2024-12-07 03:05:30 +0100

    ., ikii

--- js/scripts.js
+++ js/scripts.js
@@ -7,3 +7,12 @@ menuToggle.addEventListener('click', () => {
     menuToggle.classList.toggle('active');
     navLinksMobile.classList.toggle('active');
 });
+
+function copyContract() {
+    const contractAddress = document.getElementById('contract-address').textContent;
+    navigator.clipboard.writeText(contractAddress).then(() => {
+        alert('Contract address copied to clipboard!');
+    }).catch(err => {
+        console.error('Failed to copy: ', err);
+    });
+}
